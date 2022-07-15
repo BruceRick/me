c3e3525b1ba779a1e5da3139cd3f0a38db2443d4
Bruce Rick <brucericktgs@gmail.com>
2022-07-15 17:13:47 -0400

    Updated about component

--- src/app/core/about/about.component.ts
+++ src/app/core/about/about.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
     selector: 'app-about',
     templateUrl: './about.component.html',
     styleUrls: ['./about.component.css'],
+    encapsulation: ViewEncapsulation.None,
 })
 export class AboutComponent {
     title = 'About';
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,21 +1,30 @@
 import { NgModule } from '@angular/core';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { BrowserAnimationsModule } from    '@angular/platform-browser/animations';
+import { MatTabsModule } from '@angular/material/tabs';
 
 import { AppRoutingModule } from '../app-routing.module';
 
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { EducationComponent } from './about/education/education.component';
+
 import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
     AboutComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    EducationComponent
   ],
   imports: [
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    MatExpansionModule,
+    BrowserAnimationsModule,
+    MatTabsModule
   ],
   exports: [
     AppRoutingModule,
